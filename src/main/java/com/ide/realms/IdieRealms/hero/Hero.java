87a4eb5370e50c39a08954c82e2ad33a7567f01a
RybaLP <148885360+RybaLP@users.noreply.github.com>
2026-01-05 20:30:21 +0100

    fix: use @Builder.Default to ensure initial stats are preserved

--- src/main/java/com/ide/realms/IdieRealms/hero/Hero.java
+++ src/main/java/com/ide/realms/IdieRealms/hero/Hero.java
@@ -2,17 +2,16 @@ package com.ide.realms.IdieRealms.hero;
 
 import com.ide.realms.IdieRealms.shared.HeroClass;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Hero {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,24 +21,36 @@ public class Hero {
     private HeroClass heroClass;
 
 //    info
+    @Builder.Default
     private int level = 1;
+
+    @Builder.Default
     private long experience = 0;
+
+    @Builder.Default
     private int gold = 10;
 
 //    stats
+    @Builder.Default
     private int strength = 10;
+
+    @Builder.Default
     private int dexterity = 10;
+
+    @Builder.Default
     private int intelligence = 10;
+
+    @Builder.Default
     private int constitution = 10;
+
+    @Builder.Default
     private int luck = 10;
 
-
 //    character look
+    @Builder.Default
     private String visualConfig = "0;0;0;0;0";
 
-
 //    methods
-
     public void levelUp () {
         this.level ++;
         this.strength ++;
